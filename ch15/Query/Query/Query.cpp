8d185442c374b1702e7b5c6c334c24f26a20063d
sanbuks <sandbuks@gmail.com>
2021-11-08 18:52:25 +0800

    refactor(ch13&ch14&ch15): move ch13, 14, 15 to cpp_primer dir

--- ch15/Query/Query/Query.cpp
+++
@@ -1,7 +0,0 @@
-#include "Query.h"
-
-Query::Query(const string &s): q(new WordQuery(s)){}
-QueryResult Query::eval(const TextQuery &t) const { return q->eval(t); }
-string Query::rep() const { return q->rep(); }
-
-ostream &operator<<(ostream &os, const Query &query){ return os<<query.rep(); }

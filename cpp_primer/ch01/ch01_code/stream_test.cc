fd986d4358b03d613a87b64c0dcc1c53d1c1979a
sanbuks <sandbuks@gmail.com>
2022-03-01 14:41:58 +0800

    feat(cpp_primer/ch01): add image about io inheritance & reformat table

--- cpp_primer/ch01/ch01_code/stream_test.cc
+++ cpp_primer/ch01/ch01_code/stream_test.cc
@@ -4,9 +4,21 @@
 #include "stream_test_data_config.h"
 
 // 显示 cin 读取时状态位变化
+// eof(), bad() 可以等价显示对应状态位, 而 fail() 等价
+// ifs.rdstate() & (std::fstream::failbit | std::fstream::badbit)
+bool inline GetFailBit(std::iostream &s) {
+  return s.rdstate() & std::iostream::failbit;
+}
+
+void PrintIOStreamState(std::iostream &file) {
+  std::cout << "eof bit is " << file.eof() << "\n";
+  std::cout << "bad bit is " << file.bad() << "\n";
+  std::cout << "fail bit is " << GetFailBit(file) << "\n";
+  std::cout << "fail() is " << file.fail() << "\n";
+}
 
 int main() {
-  std::ifstream file((data_path::kStreamTestDataPath));
+  std::fstream file((data_path::kStreamTestDataPath));
   if (file) {
     int times = 0, number;
     while (file) {
@@ -14,17 +26,14 @@ int main() {
       ++times;
       std::cout << times << "\n";
       std::cout << "file stream status is : \n";
-      std::cout << "eof bit is " << file.eof() << "\n";
-      std::cout << "fail bit is " << file.fail() << "\n";
-      std::cout << "bad bit is " << file.bad() << "\n";
+      PrintIOStreamState(file);
       std::cout << "-------------------------------\n";
     }
-    std::cout << "file stream status is : \n";
-    std::cout << "eof bit is " << file.eof() << "\n";
-    std::cout << "fail bit is " << file.fail() << "\n";
-    std::cout << "bad bit is " << file.bad() << "\n";
+    std::cout << "final file stream status is : \n";
+    PrintIOStreamState(file);
   } else {
     std::cout << "can't open data file!\n";
+    exit(1);
   }
   return 0;
 }
